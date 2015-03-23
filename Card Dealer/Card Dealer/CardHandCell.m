348fabaed07e444683cda9fab545f411dab745b9
sapieneptus <cfuentes@fas.harvard.edu>
2015-03-22 20:21:25 -0400

    Refactor/cleanup

--- Card Dealer/Card Dealer/CardHandCell.m
+++ Card Dealer/Card Dealer/CardHandCell.m
@@ -15,6 +15,18 @@
 
 /* Remove current card images from subviews so they can be released */
 - (void)clearCards;
+
+/* Animates the card offscreen and removes it from its superview */
+- (void)moveCardOut:(UIImageView *)cardImage
+        destination:(CGRect)destination
+       animDuration:(float)duration;
+
+/* Moves card onscreen and tilts if facedown */
+- (void)moveCardIn:(Card *)card
+             image:(UIImageView *)cardImageView
+              xPos:(float)x
+ animationDuration:(float)duration;
+
 @end
 
 @implementation CardHandCell
@@ -32,23 +44,22 @@
             anim_constant       = CARD_EXIT_ANIM_CONSTANT,
             screenWidth         = [UIScreen mainScreen].bounds.size.width;
     
-    for (UIImageView *imageView in _cardImageViews) {
+    /* Calculate offscreen location as card's destination */
+    CGRect offscreen = CGRectMake(screenWidth + CARD_WIDTH, CARD_START_Y, CARD_WIDTH, CARD_HEIGHT);
+    
+    for (UIImageView *cardImage in _cardImageViews) {
         
         /* Calculate staggered animation delay for card */
-        float anim_delay = anim_constant + F_INVERSE(anim_delay_factor += anim_delay_factor);
+        float anim_duration = anim_constant + F_INVERSE(anim_delay_factor += anim_delay_factor);
         
-        /* Calculate offscreen location as card's destination */
-        CGRect offscreen = CGRectMake(screenWidth + CARD_WIDTH, CARD_START_Y, CARD_WIDTH, CARD_HEIGHT);
-        
-        [UIView animateWithDuration:anim_delay animations:^{
-            imageView.frame = offscreen;
-        } completion:^(BOOL finished) {
-            [imageView removeFromSuperview];
-        }];
+        /* Animate the card offscreen */
+        [self moveCardOut:cardImage destination:offscreen animDuration:anim_duration];
     }
     
     [_cardImageViews removeAllObjects];
 }
+
+#pragma mark - fillFromCards
 
 /*
  *  fillFromCards clears the current cards, 
@@ -65,39 +76,25 @@
     
     /* Layout cards left to right. 
      Facedown cards will be slanted toward the right */
-    
     float   x           = CARD_START_X,
-            y           = CARD_START_Y,
             anim_delay  = 2;
     
     for (Card *card in cards) {
-        
         /* Get the corresponding .png image */
         UIImage *cardImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@.png", card.description]];
         
         /* If Constants have been modified, it's possible there won't be an image for the card.  */
         if (cardImage) {
             UIImageView *cardImageView  = [[[UIImageView alloc] initWithImage:cardImage] autorelease];
-            
+    
             /* Start cards offscreen to the left */
-            CGRect frame                = CGRectMake(-CARD_WIDTH, y, CARD_WIDTH, CARD_HEIGHT);
+            CGRect frame                = CGRectMake(-CARD_WIDTH, CARD_START_Y, CARD_WIDTH, CARD_HEIGHT);
             cardImageView.frame         = frame;
             [self addSubview:cardImageView];
             
-            /* Now we calculate the card's onscreen position */
-            frame                       = CGRectMake(x, y, CARD_WIDTH, CARD_HEIGHT);
-            
-            /* Animate the card's entrance with a reverse stagger, such that
-                the last card dealt will move the fastest onscreen           */
-            [UIView animateWithDuration:CARD_ENTER_ANIM_CONSTANT + F_INVERSE(anim_delay += anim_delay)
-                                  delay:CARD_ENTER_ANIM_DELAY
-                                options:UIViewAnimationOptionCurveEaseOut
-                             animations:^{
-                cardImageView.frame = frame;
-                if (!card.faceup) {
-                    cardImageView.transform = CGAffineTransformMakeRotation(CARD_TILT_RADIANS);
-                }
-            } completion:nil];
+            /* Animate card onscreen */
+            float duration              = CARD_ENTER_ANIM_CONSTANT + F_INVERSE(anim_delay += anim_delay);
+            [self moveCardIn:card image:cardImageView xPos:x animationDuration:duration];
             
             /* Update x with the onscreen position of the next card */
             x += CARD_WIDTH + CARD_PADDING;
@@ -106,6 +103,41 @@
     }
 }
 
+#pragma mark - Animation
+
+- (void)moveCardIn:(Card *)card
+             image:(UIImageView *)cardImageView
+              xPos:(float)x
+ animationDuration:(float)duration {
+    
+    /* Now we calculate the card's onscreen position */
+    CGRect frame  = CGRectMake(x, CARD_START_Y, CARD_WIDTH, CARD_HEIGHT);
+    
+    /* Animate the card's entrance with a reverse stagger, such that
+     the last card dealt will move the fastest onscreen           */
+    [UIView animateWithDuration:duration
+                          delay:CARD_ENTER_ANIM_DELAY
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         cardImageView.frame = frame;
+                         if (!card.faceup) {
+                             cardImageView.transform = CGAffineTransformMakeRotation(CARD_TILT_RADIANS);
+                         }
+                     } completion:nil];
+}
+
+- (void)moveCardOut:(UIImageView *)cardImage
+        destination:(CGRect)destination
+       animDuration:(float)duration {
+    
+    [UIView animateWithDuration:duration animations:^{
+        cardImage.frame = destination;
+    } completion:^(BOOL finished) {
+        [cardImage removeFromSuperview];
+    }];
+}
+
+#pragma mark - Dealloc
 - (void)dealloc {
     [_playerNameLabel release];
     [_cardImageViews dealloc];
